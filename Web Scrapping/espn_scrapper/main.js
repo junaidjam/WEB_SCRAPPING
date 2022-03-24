0d479b218c167e9f88d74ff3088694cae6fa3de9
junaidjam <junaidakhtarjam@gmail.com>
2022-03-24 08:25:07 +0530

    espn_scrapper Start

---
+++ Web Scrapping/espn_scrapper/main.js
@@ -0,0 +1,25 @@
+let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595://www.expncricinfo.com/series/ipl-2020-21-1210595";
+const request = require("request");
+const cheerio = require("cheerio");
+
+request(url, cb);
+
+function cb(err , res , body){
+    if(err){
+        console.log("error", err);
+    }
+    else{
+        handleHTML(body);
+    }
+}
+
+function handleHTML(html){
+    let selecTool = cheerio.load(html);
+    let anchorElem = selecTool(`a[data-hover="View All Results"]`);
+    // console.log(anchorElem);
+    //attr method -> Method for getting all attributes and their values
+    let relativeLink = anchorElem.attr("href");
+    console.log(relativeLink);
+    let fullLink  = "https://www.espncricinfo.com" + relativeLink;
+    console.log(fullLink);
+}
