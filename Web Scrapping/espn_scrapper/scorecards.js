98564aba5a7380499af5e5092a97869bba17656f
junaidjam <junaidakhtarjam@gmail.com>
2022-03-24 21:57:03 +0530

    scorecard

--- Web Scrapping/espn_scrapper/scorecards.js
+++ Web Scrapping/espn_scrapper/scorecards.js
@@ -19,8 +19,16 @@ function getMatchDetails(html){
     //selectoo contains html of ith score card
     let selecTool = cheerio.load(html);
 
-    // 1. get venue
-    // 2. get date
+    //1. get team names
+    let teamNames = selecTool(".name-detail>.name-link");
+    // console.log(teamNames.text());
+
+    let team1 = selecTool(teamNames[0]).text();
+    let team2 = selecTool(teamNames[1]).text();
+    console.log("\nMATCH--->" + team1 + " V/S " + team2);
+    
+    // 2. get venue
+    // 3. get date
     let desc = selecTool(".match-header-info.match-info-MATCH")
     // console,log(desc.text());
 
@@ -28,15 +36,15 @@ function getMatchDetails(html){
     // console.log(descArr);
     let dateOfMatch = descArr[2];
     let venueOfMatch = descArr[1];
-    console.log("\nMatch ------------");
+    // console.log("\nMatch ------------");
     console.log(dateOfMatch);
     console.log(venueOfMatch);
-    // 3. get result
+    
+    // 4. get result
     let matchResEle = selecTool(
         ".match-info.match-info-MATCH.match-info-MATCH-half-width>.status-text"
       );
       console.log(matchResEle.text());
-    //4. get team names
 }
 
 module.exports = {
