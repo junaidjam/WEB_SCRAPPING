d7ff42f5f4750c41fc40efc4b1253eac88e5393b
junaidjam <junaidakhtarjam@gmail.com>
2022-03-25 00:22:33 +0530

    extract all data

--- Web Scrapping/espn_scrapper/scorecards.js
+++ Web Scrapping/espn_scrapper/scorecards.js
@@ -44,7 +44,46 @@ function getMatchDetails(html){
     let matchResEle = selecTool(
         ".match-info.match-info-MATCH.match-info-MATCH-half-width>.status-text"
       );
-      console.log(matchResEle.text());
+    console.log(matchResEle.text());
+
+    //5 . get innings
+
+    let allBatsmenTable = selecTool(".table.batsman tbody");
+    // console.log("number of batsmen tables are ->   ",allBatsmenTable.length);
+    let htmlString = "";
+    let count = 0;
+    for (let i = 0; i < allBatsmenTable.length; i++) {
+      htmlString = htmlString + selecTool(allBatsmenTable[i]).html();
+      //Get the descendants(table rows ) of each element (table )
+      let allRows = selecTool(allBatsmenTable[i]).find("tr"); // -> data of batsmen + empty rows 
+      
+      for (let i = 0; i < allRows.length; i++) {
+        //Check to see if any of the matched elements have the given className
+        let row = selecTool(allRows[i]);
+        let firstColmnOfRow = row.find("td")[0];
+        if (selecTool(firstColmnOfRow).hasClass("batsman-cell")) {
+          //will be getting valid data
+          // count++;
+          // console.log("inside " + count);
+          // name | runs | balls | 4's | 6's | sr
+          // for (let i = 0; i < 8; i++) {
+          //   if (i == 1 || i == 4) continue;
+          //   else {
+          //     console.log(selecTool(row.find("td")[i]).text());
+          //   }
+          // }
+  
+          console.log(
+            `playerName -> ${playerName}  ${runs} Runs in ${balls} balls | Hit ${numberOf4} Fours and ${numberOf6} Sixes | strikeRate-> ${sr}`
+          );
+  
+  
+        }
+      }
+    }
+  
+      
+    // console.log(htmlString);
 }
 
 module.exports = {
